2e4452db0a199a7ab3f4b7638cde42fe8771a7bd
Andrew Van <vanandrew@wustl.edu>
2018-03-15 23:50:21 -0500

    split custom functions/interfaces into separate file

---
+++ custom.py
@@ -0,0 +1,158 @@
+"""
+    Define Custom Functions and Interfaces
+"""
+from nipype.interfaces import afni,base,fsl
+
+# Define qualitycheck function
+def qualitycheck(epi,epi_sidecar,threshold=100):
+    # import necessary libraries
+    import subprocess
+    import json
+    from os.path import basename,isfile,splitext
+    # define custom indented print function
+    cprint = lambda s: print("%s%s"%("\t\t",s))
+
+    cprint("\n")
+    #TODO Is this really necessary? BIDS-validator does most of this already...
+    cprint("RUNNING QUALITY CONTROL...             ")
+    cprint("                                       ")
+    cprint("                                       ")
+    cprint("                            @@@@@      ")
+    cprint("                          @@@@@@@@     ")
+    cprint("                         @@@@@@@@      ")
+    cprint("                        @@@@@@@@       ")
+    cprint("                      @@@@@@@@         ")
+    cprint("                     @@@@@@@@          ")
+    cprint("       @@           @@@@@@@@           ")
+    cprint("     @@@@@@       @@@@@@@@             ")
+    cprint("    @@@@@@@@@    @@@@@@@@              ")
+    cprint("      @@@@@@@@@ @@@@@@@@               ")
+    cprint("        @@@@@@@@@@@@@@                 ")
+    cprint("          @@@@@@@@@@@                  ")
+    cprint("            @@@@@@@                    ")
+    cprint("              @@@@                     ")
+    cprint("                                       ")
+    cprint("                                       ")
+    cprint("Running Quality Check on File: {}".format(basename(epi)))
+
+    # call afni 3dinfo, we are checking the number of frames in the epi series
+    proc = subprocess.run('3dinfo -nv {}'.format(epi),shell=True,stdout=subprocess.PIPE)
+    # retrieve number of frames from epi series
+    try:
+        frames = int(str(proc.stdout,'utf-8'))
+        cprint('Number of frames in {} = {}.'.format(basename(epi),frames))
+    except:
+        cprint('ERROR: 3dinfo returned an invalid output on {}'.format(epi))
+        raise
+
+    # Check if the number of frames for the series did not meet the threshold
+    if frames <= threshold:
+        # series did not meet the threshold
+        cprint("Number of frames did not meet the required threshold (threshold = {})".format(threshold))
+        cprint("\n")
+        # Set output to False and return
+        return {infile: False}
+
+    # Check the bids JSON sidecar for slice timing info (Most of this stuff should already handled in the
+    # BIDS-validator, but do a check anyway just in-case...)
+    with open(epi_sidecar) as json_file:
+        # load json as dict
+        bids_sidecar = json.load(json_file)
+
+        # get slice timing
+        slice_timing = bids_sidecar['SliceTiming']
+        cprint("Slice Timing: {}".format(slice_timing))
+
+        # make sure it's defined and not an empty list
+        if slice_timing == None or slice_timing == []:
+            cprint("Slice Timing not defined in the BIDS sidecar. Check you BIDS conversion!")
+            return {'epi': epi, 'epi_sidecar': epi_sidecar, 'QC': False}
+
+    # file passed all checks, report True
+    cprint("File has cleared quality control!")
+    cprint("\n")
+    return {'epi': epi, 'epi_sidecar': epi_sidecar, 'QC': True}
+
+# Define reduce function for qualitycheck
+def QCreduceset(QClist):
+    # create lists for storing good epis
+    epi = []
+    epi_sidecar = []
+    # Create dict to store files that passed
+    for i in QClist:
+        if i['QC']: # if file passed QC, store in lists
+            epi.append(i['epi'])
+            epi_sidecar.append(i['epi_sidecar'])
+
+    # return lists of epis to process
+    return (epi, epi_sidecar)
+
+# Define class to extract slice time info and write to file
+class exslt:
+    def __init__(self,TMP_DIR):
+        self.TMP_DIR = TMP_DIR
+
+    def extract_slicetime(self,epi_sidecar):
+        # import necessary libraries
+        import json
+        import csv
+        from os.path import join,basename,splitext
+        from os import getcwd
+
+        # open the sidecar file
+        with open(epi_sidecar) as sidecar:
+            # read the sidecar
+            bids_data = json.load(sidecar)
+
+            # extract slice time information
+            slice_timing = bids_data['SliceTiming']
+
+            # extract TR
+            TR = bids_data['RepetitionTime']
+
+        # write slice timing to file
+        with open(join(self.TMP_DIR,'{}.SLICETIME'.format(splitext(basename(epi_sidecar))[0])),'w') as st_file:
+            wr = csv.writer(st_file,delimiter=' ')
+            wr.writerow(slice_timing)
+
+        # return timing pattern and TR
+        return ("@{}".format(join(self.TMP_DIR,'{}.SLICETIME'.format(splitext(basename(epi_sidecar))[0]))), str(TR))
+
+# Extend afni despike
+# define extended input spec
+class ExtendedDespikeInputSpec(afni.preprocess.DespikeInputSpec):
+    spike_file = base.File(name_template="%s_despike_SPIKES", desc='spike image file name',
+                    argstr='-ssave %s', name_source="in_file")
+
+# define extended output spec
+class ExtendedDespikeOutputSpec(afni.base.AFNICommandOutputSpec):
+    spike_file = base.File(desc='spike file', exists=True)
+
+# define extended afni despike
+class ExtendedDespike(afni.Despike):
+    input_spec = ExtendedDespikeInputSpec
+    output_spec = ExtendedDespikeOutputSpec
+
+# Extend afni volreg
+# Extend afni volreg input spec to register to specific frame
+class ExtendedVolregInputSpec(afni.preprocess.VolregInputSpec):
+    # Create class method to store IGNOREFRAMES argument and return class spec
+    @classmethod
+    def init_class(cls,IGNOREFRAMES=4):
+        # modify the base file to allow IGNOREFRAMES argument
+        cls.basefile = base.File(
+            desc='base file for registration',
+            argstr="-base %s'[{}]'".format(IGNOREFRAMES),
+            position=-6,
+            exists=True)
+
+        # return the class
+        return cls
+
+# define extended afni volreg
+class ExtendedVolreg(afni.Volreg):
+    # Create class method to initialize the input spec and return the class
+    @classmethod
+    def init_class(cls,IGNOREFRAMES):
+        cls.input_spec = ExtendedVolregInputSpec.init_class(IGNOREFRAMES)
+        return cls
--- preproc.py
+++ preproc.py
@@ -2,164 +2,22 @@
 
 import os
 from nipype import Node,MapNode,Workflow,IdentityInterface
-from nipype.interfaces import afni,base,fsl,freesurfer
+from nipype.interfaces import afni,fsl,freesurfer
 from nipype.interfaces.io import SelectFiles,DataSink
 from nipype.interfaces.utility import Function
+from custom import *
 
 """
     Settings
 """
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 SUBJECTS_DIR = os.path.join(BASE_DIR,'output','freesurfer_output')
-TMP_DIR = '/home/vana/Projects/p3/tmp'
+TMP_DIR = os.path.join(BASE_DIR,'tmp')
 IGNOREFRAMES = 4
 
 # make directories if not exist
 os.makedirs(SUBJECTS_DIR,exist_ok=True)
 os.makedirs(TMP_DIR,exist_ok=True)
-
-"""
-    Define Custom Functions and Interfaces
-"""
-
-# Define qualitycheck function
-def qualitycheck(epi,epi_sidecar,threshold=100):
-    # import necessary libraries
-    import subprocess
-    import json
-    from os.path import basename,isfile,splitext
-    # define custom indented print function
-    cprint = lambda s: print("%s%s"%("\t\t",s))
-
-    cprint("\n")
-    #TODO Is this really necessary? BIDS-validator does most of this already...
-    cprint("RUNNING QUALITY CONTROL...             ")
-    cprint("                                       ")
-    cprint("                                       ")
-    cprint("                            @@@@@      ")
-    cprint("                          @@@@@@@@     ")
-    cprint("                         @@@@@@@@      ")
-    cprint("                        @@@@@@@@       ")
-    cprint("                      @@@@@@@@         ")
-    cprint("                     @@@@@@@@          ")
-    cprint("       @@           @@@@@@@@           ")
-    cprint("     @@@@@@       @@@@@@@@             ")
-    cprint("    @@@@@@@@@    @@@@@@@@              ")
-    cprint("      @@@@@@@@@ @@@@@@@@               ")
-    cprint("        @@@@@@@@@@@@@@                 ")
-    cprint("          @@@@@@@@@@@                  ")
-    cprint("            @@@@@@@                    ")
-    cprint("              @@@@                     ")
-    cprint("                                       ")
-    cprint("                                       ")
-    cprint("Running Quality Check on File: {}".format(basename(epi)))
-
-    # call afni 3dinfo, we are checking the number of frames in the epi series
-    proc = subprocess.run('3dinfo -nv {}'.format(epi),shell=True,stdout=subprocess.PIPE)
-    # retrieve number of frames from epi series
-    try:
-        frames = int(str(proc.stdout,'utf-8'))
-        cprint('Number of frames in {} = {}.'.format(basename(epi),frames))
-    except:
-        cprint('ERROR: 3dinfo returned an invalid output on {}'.format(epi))
-        raise
-
-    # Check if the number of frames for the series did not meet the threshold
-    if frames <= threshold:
-        # series did not meet the threshold
-        cprint("Number of frames did not meet the required threshold (threshold = {})".format(threshold))
-        cprint("\n")
-        # Set output to False and return
-        return {infile: False}
-
-    # Check the bids JSON sidecar for slice timing info (Most of this stuff should already handled in the
-    # BIDS-validator, but do a check anyway just in-case...)
-    with open(epi_sidecar) as json_file:
-        # load json as dict
-        bids_sidecar = json.load(json_file)
-
-        # get slice timing
-        slice_timing = bids_sidecar['SliceTiming']
-        cprint("Slice Timing: {}".format(slice_timing))
-
-        # make sure it's defined and not an empty list
-        if slice_timing == None or slice_timing == []:
-            cprint("Slice Timing not defined in the BIDS sidecar. Check you BIDS conversion!")
-            return {'epi': epi, 'epi_sidecar': epi_sidecar, 'QC': False}
-
-    # file passed all checks, report True
-    cprint("File has cleared quality control!")
-    cprint("\n")
-    return {'epi': epi, 'epi_sidecar': epi_sidecar, 'QC': True}
-
-# Define reduce function for qualitycheck
-def QCreduceset(QClist):
-    # create lists for storing good epis
-    epi = []
-    epi_sidecar = []
-    # Create dict to store files that passed
-    for i in QClist:
-        if i['QC']: # if file passed QC, store in lists
-            epi.append(i['epi'])
-            epi_sidecar.append(i['epi_sidecar'])
-
-    # return lists of epis to process
-    return (epi, epi_sidecar)
-
-# Define function to extract slice time info and write to file
-def extract_slicetime(epi_sidecar,TMP_DIR=TMP_DIR):
-    # import necessary libraries
-    import json
-    import csv
-    from os.path import join,basename,splitext
-    from os import getcwd
-
-    # open the sidecar file
-    with open(epi_sidecar) as sidecar:
-        # read the sidecar
-        bids_data = json.load(sidecar)
-
-        # extract slice time information
-        slice_timing = bids_data['SliceTiming']
-
-        # extract TR
-        TR = bids_data['RepetitionTime']
-
-    # write slice timing to file
-    with open(join(TMP_DIR,'{}.SLICETIME'.format(splitext(basename(epi_sidecar))[0])),'w') as st_file:
-        wr = csv.writer(st_file,delimiter=' ')
-        wr.writerow(slice_timing)
-
-    # return timing pattern and TR
-    return ("@{}".format(join(TMP_DIR,'{}.SLICETIME'.format(splitext(basename(epi_sidecar))[0]))), str(TR))
-
-# Extend afni despike
-# define extended input spec
-class ExtendedDespikeInputSpec(afni.preprocess.DespikeInputSpec):
-    spike_file = base.File(name_template="%s_despike_SPIKES", desc='spike image file name',
-                    argstr='-ssave %s', name_source="in_file")
-
-# define extended output spec
-class ExtendedDespikeOutputSpec(afni.base.AFNICommandOutputSpec):
-    spike_file = base.File(desc='spike file', exists=True)
-
-# define extended afni despike
-class ExtendedDespike(afni.Despike):
-    input_spec = ExtendedDespikeInputSpec
-    output_spec = ExtendedDespikeOutputSpec
-
-# Extend afni volreg
-# Extend afni volreg input spec to register to specific frame
-class ExtendedVolregInputSpec(afni.preprocess.VolregInputSpec):
-    basefile = base.File(
-        desc='base file for registration',
-        argstr="-base %s'[{}]'".format(IGNOREFRAMES),
-        position=-6,
-        exists=True)
-
-# define extended afni volreg
-class ExtendedVolreg(afni.Volreg):
-    input_spec = ExtendedVolregInputSpec
 
 """
     Define Nodes for nipype workflow
@@ -231,11 +89,12 @@ despike2 = MapNode(
 )
 
 # extract slice timing so we can pass it to slice time correction
+slicetime = exslt(TMP_DIR) # create object
 extract_stc = MapNode(
     Function(
         input_names=['epi_sidecar'],
         output_names=['slicetiming','TR'],
-        function=extract_slicetime
+        function=slicetime.extract_slicetime
     ),
     iterfield=['epi_sidecar'],
     name='extract_stc'
@@ -264,8 +123,9 @@ tshift2 = MapNode(
 )
 
 # Motion correction
+ExVolreg = ExtendedVolreg.init_class(IGNOREFRAMES)
 volreg1 = MapNode(
-    ExtendedVolreg(
+    ExVolreg(
         args="-heptic -maxite {}".format(
             25
         ),
@@ -277,7 +137,7 @@ volreg1 = MapNode(
     name='volreg1'
 )
 volreg2 = MapNode(
-    ExtendedVolreg(
+    ExVolreg(
         args="-heptic -maxite {}".format(
             25
         ),
@@ -289,7 +149,7 @@ volreg2 = MapNode(
     name='volreg2'
 )
 volreg3 = MapNode(
-    ExtendedVolreg(
+    ExVolreg(
         args="-heptic -maxite {}".format(
             25
         ),
@@ -301,7 +161,7 @@ volreg3 = MapNode(
     name='volreg3'
 )
 volreg4 = MapNode(
-    ExtendedVolreg(
+    ExVolreg(
         args="-heptic -maxite {}".format(
             25
         ),
@@ -313,7 +173,7 @@ volreg4 = MapNode(
     name='volreg4'
 )
 volreg5 = MapNode(
-    ExtendedVolreg(
+    ExVolreg(
         args="-heptic -maxite {}".format(
             25
         ),
@@ -442,12 +302,12 @@ wf.connect([
     ]),
     # Motion Correction
     # No despike, No stc
-    # (QCreduce,volreg1,[
-    #     ('epi','basefile')
-    # ]),
-    # (QCreduce,volreg1,[
-    #     ('epi','in_file')
-    # ]),
+    (QCreduce,volreg1,[
+        ('epi','basefile')
+    ]),
+    (QCreduce,volreg1,[
+        ('epi','in_file')
+    ]),
     # despike only
     (QCreduce,volreg2,[
         ('epi','basefile')
@@ -476,40 +336,40 @@ wf.connect([
     (despike2,volreg5,[
         ('out_file','in_file')
     ]),
-    # # Skullstrip
-    # (fileselection,afni_skullstrip,[
-    #     ('T1','in_file')
-    # ]),
-    # (fileselection,fsl_skullstrip,[
-    #     ('T1','in_file')
-    # ]),
-    # # Recon-all
-    # (infosource,reconall,[
-    #     ('subject_id','subject_id')
-    # ]),
-    # (fileselection,reconall,[
-    #     ('T1','T1_files')
-    # ]),
-    # # Convert orig and brainmask
-    # (reconall,orig_convert,[
-    #     ('orig','in_file')
-    # ]),
-    # (reconall,brainmask_convert,[
-    #     ('brainmask','in_file')
-    # ]),
+    # Skullstrip
+    (fileselection,afni_skullstrip,[
+        ('T1','in_file')
+    ]),
+    (fileselection,fsl_skullstrip,[
+        ('T1','in_file')
+    ]),
+    # Recon-all
+    (infosource,reconall,[
+        ('subject_id','subject_id')
+    ]),
+    (fileselection,reconall,[
+        ('T1','T1_files')
+    ]),
+    # Convert orig and brainmask
+    (reconall,orig_convert,[
+        ('orig','in_file')
+    ]),
+    (reconall,brainmask_convert,[
+        ('brainmask','in_file')
+    ]),
     # Align T1 to ATLAS
-    # (orig_convert,allineate,[
-    #     ('out_file','in_file')
-    # ]),
-    # (fileselection,allineate,[
-    #     ('T1','reference')
-    # ]),
-    # (allineate,output1,[
-    #     ('out_matrix','output')
-    # ]),
-    # (allineate,output2,[
-    #     ('out_file','output')
-    # ])
+    (orig_convert,allineate,[
+        ('out_file','in_file')
+    ]),
+    (fileselection,allineate,[
+        ('T1','reference')
+    ]),
+    (allineate,output1,[
+        ('out_matrix','output')
+    ]),
+    (allineate,output2,[
+        ('out_file','output')
+    ])
 ])
 #wf.run()
 wf.write_graph()
